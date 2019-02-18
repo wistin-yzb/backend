56beb6d38af0c054de62057430522f8e3db35af8
git <1391930327@qq.com>
2019-02-18 18:18:46 +0800

    2019/02/18 18:18

---
+++ application/backend/controller/CheckLd.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\backend\controller;
+
+use \think\View;
+use think\Controller;
+use think\Db;
+
+class CheckLd extends controller {
+	public function __construct() {
+	}
+	
+	//index
+	public function index() {
+		/*
+		 * 解析:从数据库获取要监听的落地落地模式为自动的落地域名
+		 * 如果当前被监听的落地域名被封，则从备用落地域名列表随机获取可使用的一个域名作为替换
+		 */
+		$linelist = db ( 'line' )->where ("status",1)->field('id,name')->select ();		
+		if($linelist){	
+			$lineids = []; 
+			foreach ($linelist as $key=>$val){
+				$lineids[] = $val['id'];
+			}		
+			$lineidsstr = implode(',', $lineids);
+			$where = "s.is_active=1 and s.d1_model=2 and s.line_id in ($lineidsstr)"; //对已启用的落地模式为自动的服务器进行监听
+			$serverlist = db ( 'server' )->alias('s')->where ($where)->field('s.id,s.name,l.name as line_name,s.d1,s.public_ip,s.line_id')->join('line l','s.line_id = l.id')->select ();						
+			
+			if($serverlist){
+				foreach ($serverlist as $key_s=>$val_s){				 	
+				 	//查询指定域名列表是否被封
+					self::check_domain($val_s);
+				 }
+			}
+		}
+	}
+	
+	//微信域名检测宝盒接口
+	public function check_domain($info){
+		if(!$info){return;}
+		$user = "293047b";
+		$key = "4f187f5f2ce23ed2b7754d8d54b8f1f3";
+		$forbbiden = array();
+		
+		$api_url = "http://vip.weixin139.com/weixin/wx_domain.php?user=$user&key=$key&domain=".$info['d1'];
+		$content = $this->http_post($api_url);
+		$data = json_decode($content,true);
+		if($data['status']==2){ //域名被封
+			$filename = "forbbiden/{$info['d1']}.txt";
+			if(!file_exists($filename)){
+				file_put_contents($filename,$info['d1']);
+				$this->send_sms($info);
+			}
+		}else{
+			var_dump ( 'luodi domain is ok!' );
+			exit ();
+		}
+	}
+	
+	//http_post
+	public function http_post($url){
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_TIMEOUT,5);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+		$data = curl_exec($ch);
+		if($data){
+			curl_close($ch);
+			return $data;
+		}else {
+			$error = curl_errno($ch);
+			curl_close($ch);
+			return false;
+		}
+	}
+	
+	//短信通知
+	public function send_sms($info){
+		if(!$info){
+			return;
+		}
+		$where = "line_id = {$info['line_id']} and status=1";	//状态，1可用，2不可用
+		$luodi_domain = db ( 'luodi' )->where ($where)->field('domain,line_id,status')->select ();
+		$tmpArr = array();
+		if($luodi_domain){
+			foreach ($luodi_domain as $key=>$val){
+				$tmpArr[] = $val['domain'];
+			}
+			$randkey = array_rand($tmpArr); //新的随机落地域名
+			$randdomain = $tmpArr[$randkey];
+			$remainnum = count($tmpArr)-1;//剩余备用落地域名个数
+						
+			//更新当前服务器落地域名,并且更新落地域名状态
+			db ( 'luodi' )->where ($where)->field('domain,line_id,status')->select ();
+			db ( 'server' )->where ( "id", '=', $info['id'] )->update ( array('d1'=>$randdomain) );
+			db ( 'luodi' )->where ( "domain", '=', $randdomain)->update ( array('status'=>2,'update_time'=>time()) );
+			
+			$content = "【来自火星的运维】您好，您的服务器<<{$info['line_name']}-{$info['name']}>>落地域名<<{$info['d1']}>>被封禁，现自动切换到<<$randdomain>>成功！剩余备用域名{$remainnum}个.";
+			//$content = "【来自火星的运维】您好，您的服务器{$info['d1']}(域名)已经被封禁！";
+			require  './lib/SUBMAIL_PHP_SDK-master/app_config.php';
+			require_once './lib/SUBMAIL_PHP_SDK-master/SUBMAILAutoload.php';
+
+			#1条API请求发送单个号码
+			/* $submessage  =new \MESSAGEsend($message_configs);
+			$submessage->setTo('15812454795');
+			$submessage->SetContent($content);
+			$result =$submessage->send(); */
+			
+			#1条API请求发送多个号码,建议:单线程提交数量控制在50个联系人, 可以开多个线程同时发送
+			$submail=new \MESSAGEMultiSend($message_configs);			
+			$contacts=array("15812454795");			
+			foreach($contacts as $contact){
+				$multi=new \Multi();
+				$multi->setTo($contact);
+				$submail->addMulti($multi->build());
+			}			
+			$submail->SetContent($content);			
+			$result = $submail->multisend();
+			echo '<pre>'; 
+			var_dump($result);
+			echo '</pre>';
+			
+		}
+	}
+	
+}
--- application/backend/controller/Index.php
+++ application/backend/controller/Index.php
@@ -93,221 +93,81 @@ class Index extends controller {
 	
 	//群分享、圈分享、总量、分享率统计
 	public function statistics(){
-		action('Common/checkSession');
-		//获取所有案例的统计信息
-		$where = 'id>0 and status=1';
-		$list = db ( 'line' )->where ( $where )->field('name,state_ip')->order('id','desc')->select ();		
-		//$jsonreturn = '{"data":{"抽签26":{"friends":[{"value":1,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":2,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":6,"label":{"show":true}},{"value":3,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":3,"label":{"show":true}},{"value":5,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":3,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":1,"label":{"show":true}},{"value":2,"label":{"show":true}},{"value":2,"label":{"show":true}},{"value":3,"label":{"show":true}},{"value":0,"label":{"show":true}}],"timeline":[{"value":122,"label":{"show":true}},{"value":113,"label":{"show":true}},{"value":118,"label":{"show":true}},{"value":155,"label":{"show":true}},{"value":220,"label":{"show":true}},{"value":245,"label":{"show":true}},{"value":242,"label":{"show":true}},{"value":265,"label":{"show":true}},{"value":151,"label":{"show":true}},{"value":49,"label":{"show":true}},{"value":27,"label":{"show":true}},{"value":36,"label":{"show":true}},{"value":24,"label":{"show":true}},{"value":29,"label":{"show":true}},{"value":59,"label":{"show":true}},{"value":137,"label":{"show":true}},{"value":225,"label":{"show":true}},{"value":221,"label":{"show":true}},{"value":197,"label":{"show":true}},{"value":135,"label":{"show":true}},{"value":109,"label":{"show":true}},{"value":114,"label":{"show":true}},{"value":118,"label":{"show":true}},{"value":121,"label":{"show":true}},{"value":126,"label":{"show":true}},{"value":7,"label":{"show":true}}],"total":[{"value":123,"label":{"show":true}},{"value":113,"label":{"show":true}},{"value":118,"label":{"show":true}},{"value":155,"label":{"show":true}},{"value":222,"label":{"show":true}},{"value":245,"label":{"show":true}},{"value":248,"label":{"show":true}},{"value":268,"label":{"show":true}},{"value":152,"label":{"show":true}},{"value":49,"label":{"show":true}},{"value":28,"label":{"show":true}},{"value":36,"label":{"show":true}},{"value":24,"label":{"show":true}},{"value":30,"label":{"show":true}},{"value":59,"label":{"show":true}},{"value":138,"label":{"show":true}},{"value":228,"label":{"show":true}},{"value":226,"label":{"show":true}},{"value":198,"label":{"show":true}},{"value":138,"label":{"show":true}},{"value":109,"label":{"show":true}},{"value":115,"label":{"show":true}},{"value":120,"label":{"show":true}},{"value":123,"label":{"show":true}},{"value":129,"label":{"show":true}},{"value":7,"label":{"show":true}}],"ratio":[{"value":"34.17","label":{"show":false}},{"value":"40.07","label":{"show":false}},{"value":"34.20","label":{"show":false}},{"value":"35.47","label":{"show":false}},{"value":"36.94","label":{"show":false}},{"value":"37.07","label":{"show":false}},{"value":"35.48","label":{"show":false}},{"value":"43.72","label":{"show":false}},{"value":"33.78","label":{"show":false}},{"value":"32.67","label":{"show":false}},{"value":"34.57","label":{"show":false}},{"value":"60.00","label":{"show":false}},{"value":"48.98","label":{"show":false}},{"value":"18.75","label":{"show":false}},{"value":"31.72","label":{"show":false}},{"value":"33.33","label":{"show":false}},{"value":"35.24","label":{"show":false}},{"value":"34.45","label":{"show":false}},{"value":"33.73","label":{"show":false}},{"value":"23.15","label":{"show":false}},{"value":"32.44","label":{"show":false}},{"value":"34.74","label":{"show":false}},{"value":"31.09","label":{"show":false}},{"value":"33.24","label":{"show":false}},{"value":"28.54","label":{"show":false}},{"value":"41.18","label":{"show":false}}]},"视频25":{"friends":[{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}}],"timeline":[{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}}],"total":[{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}},{"value":0,"label":{"show":true}}],"ratio":[{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":0,"label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}},{"value":0,"label":{"show":false}},{"value":0,"label":{"show":false}},{"value":"0.00","label":{"show":false}},{"value":0,"label":{"show":false}}]}},"key":["抽签26","视频25"],"hour":[15,16,17,18,19,20,21,22,23,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]}';
+		action('Common/checkSession');		
+		$servers = Db::name('line')->where('status', 1)->order('id', 'desc')->limit(5)->select();		
 		
-		$data = array();
-		$key = array();
-		$hour = array();
-		
-		if($list){
-			foreach ($list as $k=>$v){
-				$key[] = $v['name'];
-// 				$rurl = "http://{$v['state_ip']}/r.txt";
-// 				$data[$k][$v['name']]= $this->getall($rurl);
+		$lines = [];
+		if ($servers) {
+			// 超时限定
+			$opts = stream_context_create(array(
+					'http' => array(
+							'timeout' => 1   // Timeout in seconds
+					)
+			));		
+			
+			// 时间下标
+			$now = date('H');
+			$today = range(0, $now);
+			$lastDay = $now > 0 ? range($now-1, 23) : range($now, 23);
+			$hour = array_merge($lastDay, $today);
+			
+			foreach ($servers as $server) {
+				//群分享
+				$friend = [];
+				// 圈分享
+				$timeline = [];
+				// total
+				$total = [];
+				// 比例
+				$ratio = [];
+				
+				// 分析数据
+				@$json = !empty($server['state_ip']) ? file_get_contents("http://{$server['state_ip']}/r.txt", false, $opts) : '';
+				if ($json != '') {
+					$arr = json_decode($json, true);
+				} else {
+					$arr = array();
+				}
+				// 访问数据
+				@$visite = !empty($server['state_ip']) ? file_get_contents("http://{$server['state_ip']}/r_view.txt", false, $opts) : '';
+				if ($visite != '') {
+					$visiteArr = json_decode($visite, true);
+				} else {
+					$visiteArr = array();
+				}
+				$count = 1;
+				foreach ($hour as $key => $item) {
+					if ($count <= count($lastDay)) {
+						$index = strtotime(date('Y-m-d ',strtotime('-1 day')) . $item . ':0:0');
+					}  else {
+						$index = strtotime(date('Y-m-d ') . $item . ':0:0');
+					}
+					$count++;
+					$friend[$key]['value'] = isset($arr['friend'][$index]) ? $arr['friend'][$index] : 0;
+					$friend[$key]['label'] = ['show' => true];
+					$timeline[$key]['value'] = isset($arr['timeline'][$index]) ? $arr['timeline'][$index] : 0;
+					$timeline[$key]['label'] = ['show' => true];
+					$total[$key]['value'] = $friend[$key]['value'] + $timeline[$key]['value'];
+					$total[$key]['label'] = ['show' => true];
+					
+					$ratio[$key]['value'] = (empty($visiteArr) || !isset($visiteArr['baba'][$index]) || empty($visiteArr['baba']) || empty($visiteArr['baba'][$index])) ? 0 : (sprintf('%.2f', (($friend[$key]['value'] + $timeline[$key]['value']) * 100) / $visiteArr['baba'][$index]));
+					$ratio[$key]['label'] = ['show' => false];
+					if (100 <= $ratio[$key]['value']) {
+						$ratio[$key]['value'] = 100;
+					}
+					
+				}
+				$lines[$server['name']] = [
+						'friend' => $friend,
+						'timeline' => $timeline,
+						'total' => $total,
+						'ratio' => $ratio
+				];
 			}
+			return json(['data' => $lines, 'key' => array_keys($lines), 'hour' => $hour]);
+		} else {
+			return json([]);
 		}
-// 		echo '<pre>';
-// 		var_dump($data);exit;
 		
-		$data = array(
-			'案例1'=>array(
-					'friend'=>array(
-							array("value"=>1),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-					'timeline'=>array(
-							array("value"=>2),
-							array("value"=>3),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-					'total'=>array(
-							array("value"=>3),
-							array("value"=>3),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-			),
-			'案例2'=>array(
-					'friend'=>array(
-							array("value"=>21),
-							array("value"=>23),
-							array("value"=>24),	
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-					'timeline'=>array(
-							array("value"=>3),
-							array("value"=>4),
-							array("value"=>9),	
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-					'total'=>array(
-							array("value"=>24),
-							array("value"=>27),
-							array("value"=>33),	
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-							array("value"=>0),
-					),
-			),
-		);
-		$hour =$this->get24hourArry();
-		$jsonreturn = json_encode(array(
-				"data"=>$data,
-				"key"=>$key,
-				"hour"=>$hour,
-		));
-		exit($jsonreturn);		
 	}
-	
-	//获取全部记录数据
-	public  function getall($fileurl){
-		$json = file_get_contents($fileurl);		
-		if($json != '')
-		{
-			$arr = json_decode($json,true);
-		}else{
-			$arr = array();
-		}		
-		$timeline = $arr['timeline'];
-		$friend  = $arr['friend'];
-		
-		$sum1 = $timeline[0];
-		$sum2 = $friend[0];
-		
-		unset($timeline[0]);
-		unset($friend[0]);
-		
-		$max = strtotime(date('Y-m-d H:00:00')); //当前时间		
-		
-		$min1 = current(array_keys($timeline));
-		$min2 = current(array_keys($friend));
-		$min = $min1 >= $min2 ? $min1 : $min2;
-		
-		if($min1 != $min2)
-		{
-			foreach($timeline as $key => $value)
-			{
-				if($key < $min)
-				{
-					unset($timeline[$key]);
-				}
-			}
-			foreach($friend as $key => $value)
-			{
-				if($key < $min)
-				{
-					unset($friend[$key]);
-				}
-			}
-		}
-		
-		$long = 25;
-		if($max > $min + $long * 60 * 60)
-		{			
-			$min = $max - $long * 60 * 60;
-			foreach($timeline as $key => $value)
-			{
-				if($key < $min)
-				{
-					unset($timeline[$key]);
-				}
-			}
-			foreach($friend as $key => $value)
-			{
-				if($key < $min)
-				{
-					unset($friend[$key]);
-				}
-			}
-		}
-		
-		$keys = array();
-		$all = array();
-		$len = ($max - $min) / (60 * 60);
-		for($i = 0; $i <= $len; $i ++)
-		{
-				$key = $min + $i * 60 * 60;
-				$keys[$i] = $key;
-				if(!isset($timeline[$key]))
-				{
-					$timeline[$key] = 0;
-				}
-				if(!isset($friend[$key]))
-				{
-					$friend[$key] = 0;
-				}
-				ksort($timeline);
-				ksort($friend);
-				$all[$key] = $timeline[$key] + $friend[$key];
-		}
-		return array(
-				'friend'=>$friend,
-				'timeline'=>$timeline,
-				'total'=>$all,
-				'keys'=>$keys				
-		);
-}
-
-//获取当前时间的前24个小时
-public function get24hourArry(){
-	$sysh = date('H');
-	$sysh1 = $sysh-1>=0?$sysh-1:24-1;
-	$sysh2 = $sysh-2>=0?$sysh-2:24-2;
-	$sysh3 = $sysh-3>=0?$sysh-3:24-3;
-	$sysh4 = $sysh-4>=0?$sysh-4:24-4;
-	$sysh5 = $sysh-5>=0?$sysh-5:24-5;
-	$sysh6 = $sysh-6>=0?$sysh-6:24-6;
-	$sysh7 = $sysh-7>=0?$sysh-7:24-7;
-	$sysh8 = $sysh-8>=0?$sysh-8:24-8;
-	$sysh9 = $sysh-9>=0?$sysh-9:24-9;
-	$sysh10 = $sysh-10>=0?$sysh-10:24-10;
-	return array($sysh10,$sysh9,$sysh8,$sysh7,$sysh6,$sysh5,$sysh4,$sysh3,$sysh2,$sysh1,$sysh);
-}
 }
