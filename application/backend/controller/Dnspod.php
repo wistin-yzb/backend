62563094f5ea986283464cfbfc776a45f648059f
git <1391930327@qq.com>
2019-02-23 12:06:49 +0800

    noon push

--- application/backend/controller/Dnspod.php
+++ application/backend/controller/Dnspod.php
@@ -8,16 +8,7 @@ use think\Db;
 class Dnspod extends controller {
 	public function __construct() {
 		import('dnspod-api-php-web-master.dnspod',VENDOR_PATH);		
-		 
 		@session_start();
-	}
-	
-	//dnspod_list
-	public function dnspod_list() {
-		$post = input ( 'post.' );
-		$keywords = @$post ['keywords'] ? trim ( @$post ['keywords'] ) : '';
-		$view = new View ();
-		$dnspod = new \dnspod();	
 		$_POST['token_id'] = "77302";
 		$_POST['token_key'] = "8d81b469908cffdb4e9191552a965743";
 		if ($_POST['token_id'] == '') {
@@ -33,13 +24,29 @@ class Dnspod extends controller {
 			}
 		} else {
 			$_SESSION['token_key'] = $_POST['token_key'];
-		}		
-		$_SESSION['cookies'] = time();
+		}
+		$_SESSION['cookies'] = time();		
+	}
+	
+	//dnspod_list
+	public function dnspod_list() {
+		$post = input ( 'post.' );
+		$keywords = @$post ['keywords'] ? trim ( @$post ['keywords'] ) : '';
+		$view = new View ();
+		$dnspod = new \dnspod();			
+		//核查域名是否在当前账号里面
+		$checkdomain = $dnspod->api_call('Domain.Info', array('domain'=>$keywords));			
+		if(!$checkdomain){
+			$view->is_exists = false;
+		}else{
+			$view->is_exists = true;
+		}
 		$postArr = [
 								'domain' => $keywords,
 								'record_type' => 'A'
 						];
 		$response = $dnspod->api_call('Record.List', $postArr);			
+		
 		if($response){
 			$view->list = $response['records'];			
 			$filter = [
@@ -53,26 +60,57 @@ class Dnspod extends controller {
 					'total' => count (array())
 			];
 		}
-	
 		$view->filter = $filter;
 		return $view->fetch ( 'dnspod/dnspod_list' );
 	}
 	
 	//record_add
-	public function dnspod_add() {		
-		$input = input ( 'get.' );
-		$view = new View ();
+	public function dnspod_add() {			
+		$input = input ( 'get.' );		
+		$view = new View ();		
 		$view->domain = $input['domain'];
 		return $view->fetch ( 'dnspod/dnspod_add' );
 	}
 	
 	//dnspod_sumbit
 	public function dnspod_sumbit() {		
-		exit(json_encode(1));
+		$post = input ( 'post.' );
+		if(!$post){
+			exit(json_encode(-2));
+		}
+		$postArr = array(
+				'domain'=>$post['domain'],
+				'sub_domain'=>$post['sub_domain'],
+				'value'=>$post['value'],
+				'record_type'=>$post['record_type'],
+				'record_line'=>$post['record_line'],
+				'ttl'=>$post['ttl'],
+		);
+		$dnspod = new \dnspod();	
+		$response = $dnspod->api_call('Record.Create', $postArr);
+		if($response['status']['code']==1){			
+			exit(json_encode(1));
+		}else{
+			exit(json_encode(-1));
+		}
 	}
 	
 	//dnspod_del
 	public function dnspod_del() {
-		exit(json_encode(1));
+		$post = input ( 'post.' );
+		if(!$post){
+			exit(json_encode(-2));
+		}
+		$dnspod = new \dnspod();
+		$postArr = array(
+				'domain'=>$post['domain'],
+				'record_id'=>$post['record_id'],
+		);
+		$response = $dnspod->api_call('Record.Remove', $postArr);
+		if($response['status']['code']==1){
+			exit(json_encode(1));
+		}else{
+			exit(json_encode(-1));
+		}
 	}
 }
